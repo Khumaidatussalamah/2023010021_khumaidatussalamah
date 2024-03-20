db21bf1cade9121a349949d8d9a358220718f37a
Khumaidatussalamah <khumaidatussalamah@gmail.com>
2024-03-19 20:30:46 -0800

    firstcome

---
+++ Main.js
@@ -0,0 +1,7 @@
+let nama = 'khumaidatus' // tipe string
+let usia = 20 // tipe integer number
+let tinggiBadan = 173.5 // tipe double float 
+let beratBadan
+let pacar = null
+
+alert(`nama saya ${nama} usia saya itu ${usia} tinggi badan saya adalah ${tinggiBadan}`)
